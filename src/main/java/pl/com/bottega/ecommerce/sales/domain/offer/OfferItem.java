325548d9e2753ba7f08252abb3aa2ed1296375ba
Kamil Żarski <tankisgreat@gmail.com>
2019-03-13 19:11:21 +0100

    Change primitive type to Referance type, change Constructor, remove accesore

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -18,93 +18,33 @@ import java.util.Objects;
 
 public class OfferItem {
 
-    // product
-    private String productId;
+    private ProductData product;
 
-    private BigDecimal productPrice;
+    private Discount discount;
 
-    private String productName;
-
-    private Date productSnapshotDate;
-
-    private String productType;
+    private Money totalCost;
 
     private int quantity;
 
-    private BigDecimal totalCost;
-
-    private String currency;
-
-    // discount
-    private String discountCause;
-
-    private BigDecimal discount;
-
-    public OfferItem(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate, String productType,
-            int quantity) {
-        this(productId, productPrice, productName, productSnapshotDate, productType, quantity, null, null);
+    public OfferItem(ProductData product) {
+        this(product, 0, new Discount("", new Money(BigDecimal.ZERO, "PLN")));
     }
 
-    public OfferItem(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate, String productType,
-            int quantity, BigDecimal discount, String discountCause) {
-        this.productId = productId;
-        this.productPrice = productPrice;
-        this.productName = productName;
-        this.productSnapshotDate = productSnapshotDate;
-        this.productType = productType;
-
-        this.quantity = quantity;
+    public OfferItem(ProductData product, int quantity, Discount discount) {
+        this.product = product;
         this.discount = discount;
-        this.discountCause = discountCause;
-
-        BigDecimal discountValue = new BigDecimal(0);
-        if (discount != null) {
-            discountValue = discountValue.subtract(discount);
-        }
-
-        this.totalCost = productPrice.multiply(new BigDecimal(quantity))
-                                     .subtract(discountValue);
+        this.quantity = quantity;
+        this.totalCost = new Money(product.getPrice().getValue().subtract(discount.getDiscount().getValue()), "PLN");
     }
 
-    public String getProductId() {
-        return productId;
-    }
-
-    public BigDecimal getProductPrice() {
-        return productPrice;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public Date getProductSnapshotDate() {
-        return productSnapshotDate;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public BigDecimal getTotalCost() {
-        return totalCost;
-    }
-
-    public String getTotalCostCurrency() {
-        return currency;
-    }
-
-    public BigDecimal getDiscount() {
-        return discount;
-    }
-
-    public String getDiscountCause() {
-        return discountCause;
+    public ProductData getProduct() {
+        return product;
     }
 
     public int getQuantity() {
         return quantity;
     }
+
 
     @Override
     public int hashCode() {
